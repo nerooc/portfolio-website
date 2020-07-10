3af7856a2c42b8f74c838d0b44073b0788b939c3
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-07-10 12:13:10 +0200

    react-reveal implemented, landing page bugfixes
    added react-reveal to the ITberries section

--- src/Components/ITberries/ITberries.js
+++ src/Components/ITberries/ITberries.js
@@ -4,6 +4,7 @@ import './ITberries.css';
 import Button from '../Button/Button';
 import itbPNG from '../../Images/ITB.png';
 import Parallax from 'react-rellax';
+import Fade from 'react-reveal/Fade';
 
 class Header extends React.Component {
 	render() {
@@ -13,18 +14,24 @@ class Header extends React.Component {
 					<img className="itb-logo" src={itbPNG} alt="itb-logo" />
 				</Parallax>
 				<div className="text-container">
-					<h1 className="h1white">IT BERRIES</h1>
-					<p className="itb-p">
-						Nulla in velit a metus rhoncus tempus. Nulla congue nulla vel sem
-						varius finibus. Sed ornare sit amet lorem sed viverra. In vel urna
-						quis libero viverra facilisis ut ac est. Morbi commodo, eros in
-						dignissim tempus, lacus odio rutrum augue, in semper sem magna quis
-						tellus. Etiam enim erat, suscipit eu semper a, dictum sit amet elit.
-						Nunc egestas nisi eget enim gravida facilisis. Pellentesque laoreet
-						varius turpis vel pharetra. Ut ante justo, consequat vitae elementum
-						tempor, accumsan nec eros.
-					</p>
-					<Button text="READ MORE" />
+					<Fade left duration={2500} distance="30px">
+						<h1 className="h1white">IT BERRIES</h1>
+					</Fade>
+					<Fade left duration={2500} distance="30px">
+						<p className="itb-p">
+							Nulla in velit a metus rhoncus tempus. Nulla congue nulla vel sem
+							varius finibus. Sed ornare sit amet lorem sed viverra. In vel urna
+							quis libero viverra facilisis ut ac est. Morbi commodo, eros in
+							dignissim tempus, lacus odio rutrum augue, in semper sem magna
+							quis tellus. Etiam enim erat, suscipit eu semper a, dictum sit
+							amet elit. Nunc egestas nisi eget enim gravida facilisis.
+							Pellentesque laoreet varius turpis vel pharetra. Ut ante justo,
+							consequat vitae elementum tempor, accumsan nec eros.
+						</p>
+					</Fade>
+					<Fade left duration={2500} distance="30px">
+						<Button text="READ MORE" />
+					</Fade>
 				</div>
 			</div>
 		);
