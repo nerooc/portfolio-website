3e3516bb651773a58e79d9a7e60256cce54bb703
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-07-22 19:59:48 +0200

    added seperator for testing and responsivness adjustments
    to do: probably a new seperator svg

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,6 +3,7 @@ import './Header.css';
 import mailPNG from '../../Images/mail.png';
 import githubPNG from '../../Images/github.png';
 import linkedinPNG from '../../Images/linked-in.png';
+import separator from '../../Images/separatorWhite.png';
 
 class Header extends React.Component {
 	render() {
@@ -10,15 +11,17 @@ class Header extends React.Component {
 			<div className="container">
 				<h2>my name is Thomas</h2>
 				<h1>I'M A DEVELOPER</h1>
+				<img src={separator} className="separator" alt="separator" />
+
 				<div className="social-media">
 					<a href="#">
-						<img src={mailPNG} alt="mail-svg" />
+						<img className="social-media-unit" src={mailPNG} alt="mail-svg" />
 					</a>
 					<a href="#">
-						<img src={githubPNG} alt="git-svg" />
+						<img className="social-media-unit" src={githubPNG} alt="git-svg" />
 					</a>
 					<a href="#">
-						<img src={linkedinPNG} alt="linked-in-svg" />
+						<img className="social-media-unit" src={linkedinPNG} alt="linked-in-svg" />
 					</a>
 				</div>
 			</div>
