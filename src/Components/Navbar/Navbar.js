a50c8c71d2c320630ed09249ca1d01842dee186d
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-08-14 00:15:55 +0200

    added logo to navbar (used peace sign for now)

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Navbar.css';
-import hamburger from '../../Images/hamburger.svg';
+import logo from '../../Images/peace.svg';
 import '../../globalStyles/hamburger.css';
 
 class Navbar extends React.Component {
+
+    // metoda obsługująca menu hamburger, zrzuca i chowa dropdown menu
 
     handleHamburgerClick = () => {
         const hamburger = document.querySelector('.hamburger');
@@ -13,21 +15,38 @@ class Navbar extends React.Component {
             .classList
             .toggle('nav-collapse');
 
-        hamburger.classList.toggle('is-active');
-        
+        hamburger
+            .classList
+            .toggle('is-active');
+
     }
 
     render() {
+
+        // funkcja która po scrollu przesuwa navbar w górę i dołącza czarny background
+
         window.onscroll = () => {
             const nav_wrap = document.querySelector('.nav-wrapper');
-            if (this.scrollY <= 10) 
-                nav_wrap.classList.remove('scroll-wrapper');
-            else 
+            const logo = document.querySelector('.logo');
+
+            if (this.scrollY <= 10) {
+                nav_wrap
+                    .classList
+                    .remove('scroll-wrapper');
+                logo
+                    .classList
+                    .remove('logo-wrapper');
+            } else {
                 nav_wrap
                     .classList
                     .add('scroll-wrapper');
-            
+                logo
+                    .classList
+                    .add('logo-wrapper');
+            }
+
             const nav_links = document.querySelector('.nav-links');
+
             if (this.scrollY <= 10) 
                 nav_links.classList.remove('scroll-links');
             else 
@@ -35,16 +54,25 @@ class Navbar extends React.Component {
                     .classList
                     .add('scroll-links');
             
-            if (window.scrollY === 0) 
-                nav_wrap.classList.remove('scroll-wrapper');
-            if (window.scrollY === 0) 
-                nav_links.classList.remove('scroll-links');
-            };
-        
+            if (window.scrollY === 0) {
+                logo
+                    .classList
+                    .remove('logo-wrapper');
+                nav_wrap
+                    .classList
+                    .remove('scroll-wrapper');
+                nav_links
+                    .classList
+                    .remove('scroll-links');
+            }
+
+        };
+
         return (
             <header>
 
                 <div className="nav-wrapper">
+                    <img className="logo" src={logo}/>
                     <div className="black-bar">
                         <button
                             className="hamburger hamburger--collapse"
