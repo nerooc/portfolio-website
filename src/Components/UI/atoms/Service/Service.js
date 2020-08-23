6371820669ba0138d45abfd30cacbf73e6c0651f
Tomasz Gajda <tomasz_gajda@outlook.com>
2020-08-23 14:44:05 +0200

    adding new component - service

---
+++ src/Components/UI/atoms/Service/Service.js
@@ -0,0 +1,12 @@
+import react from React;
+import "./Service.css";
+
+const service = (props) => {
+    return (
+        <div>
+            <img />
+            <h1></h1>
+            <p></p>
+        </div>
+    );
+}
