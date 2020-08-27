c6a978d674b05d10fc6e9e15c7ca5416fd0b02d9
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-08-27 11:01:32 +0200

    updated components, testing more fade-in effects

--- src/Components/UI/organisms/About/About.js
+++ src/Components/UI/organisms/About/About.js
@@ -5,18 +5,26 @@ import Button from '../../atoms/Button/Button';
 import Services from '../../molecules/Services/Services';
 import separator from '../../../../Images/separatorBlack.png';
 
+import Fade from 'react-reveal/Fade';
+
 
 import './About.css';
 
 const About = () => {
 	return (
         <div className="about">
-            <SectionHeader className="about__header">ABOUT ME</SectionHeader>
-           
-            <Paragraph className="about__paragraph">My name is Tomasz Gajda, I’m a third year Applied Computer Science student at AGH University of Science and Technology in Cracow, Poland. </Paragraph>
-            
-            <Button className="about__button">EXPLORE</Button>
-            
+
+            <Fade top duration={2500} distance="30px">
+                <SectionHeader className="about__header">ABOUT ME</SectionHeader>
+            </Fade>
+
+                <Paragraph className="about__paragraph">
+                    My name is Tomasz Gajda, I’m a third year Applied Computer Science student at AGH University of Science and Technology in Cracow, Poland. 
+                </Paragraph>
+                <Fade bottom duration={2500} distance="30px">
+
+                    <Button className="about__button">EXPLORE</Button>
+                </Fade>
             <img src={separator} className="about__separator" alt="aboutSeparator" />
             
             <Services />
