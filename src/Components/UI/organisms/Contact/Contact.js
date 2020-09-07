bf899a6adf48eccd9ad04577764ed7ec1f7e6ad7
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-09-07 19:35:15 +0200

    added paragraph to the contact section

--- src/Components/UI/organisms/Contact/Contact.js
+++ src/Components/UI/organisms/Contact/Contact.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import SectionHeader from '../../atoms/SectionHeader/SectionHeader';
+import Paragraph from '../../atoms/Paragraph/Paragraph';
 
 import './Contact.css';
 
 const Contact = (props) => {
 	return (
 			<div className="contact">
-				<SectionHeader>CONTACT</SectionHeader>
+				<SectionHeader className="contact__header">CONTACT</SectionHeader>
+
+				<Paragraph className="contact__paragraph">My name is Tomasz Gajda, I’m a third year Applied Computer Science student at AGH University of Science and Technology in Cracow, Poland. </Paragraph>
 			</div>
 	);
 }
