9e8783096df4bb76c17dd820a5f34a8d2c2f0ac6
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-09-14 11:20:43 +0200

    added emailJS which allows sending mails through a form (added success/error message)

--- src/Components/UI/organisms/Contact/Contact.js
+++ src/Components/UI/organisms/Contact/Contact.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
+import { template, userId } from '../../../../keys';
+
 import SectionHeader from '../../atoms/SectionHeader/SectionHeader';
 import Paragraph from '../../atoms/Paragraph/Paragraph';
 import Input from '../../atoms/Input/Input';
 import Button from '../../atoms/Button/Button';
 import separator from '../../../../Images/separatorBlack.png';
+
 
 import './Contact.css';
 
@@ -45,7 +49,14 @@ class Contact extends React.Component {
                 },
                 value: ''
             }
+        },
+
+        email: {
+            emailBeingSent: false,
+            emailSent: false,
+            emailSuccess: null
         }
+        
     }
 
     inputChangeHandler = (event, elementID) => {
@@ -72,10 +83,32 @@ class Contact extends React.Component {
             number:this.state.formConfig.number.value,
             message: this.state.formConfig.message.value
         }
-        
-        /* email sending */
-        console.log(mail);
 
+        this.setState({
+            email: {
+                emailBeingSent: true
+            }
+        })
+
+        emailjs.send('Gmail', template, mail, userId)
+            .then((result) => {
+                this.setState({
+                    email: {
+                        emailBeingSent: true,
+                        emailSent: true,
+                        emailSuccess: true
+                    }
+                })
+            }, (error) => {
+                this.setState({
+                    email: {
+                        emailBeingSent: true,
+                        emailSent: true,
+                        emailSuccess: false
+                    }
+                })
+                console.log(error.text);
+            });
     }
 
     render() {
@@ -87,7 +120,7 @@ class Contact extends React.Component {
             });
 
         const form = (
-            <>
+            <div className="contact__form">
                 {contactForm.map((formElement => {
                     return <Input
                         key={formElement.id}
@@ -97,28 +130,44 @@ class Contact extends React.Component {
                         inputConfig={formElement.element.config}
                         value={formElement.element.value}/>;
                 }))}
-            </>
+            </div>
         );
         
+        let emailResult = null;
+
+        if(this.state.email.emailBeingSent){
+            emailResult = <div className="contact__email"> The email is being sent... </div>;
+
+            if (this.state.email.emailSent) {
+                emailResult = this.state.email.emailSuccess
+                    ? <div className="contact__email emailSent">
+                            The email was sent successfully!
+                        </div>
+                    : <div className="contact__email emailError">
+                        There has been an error when sending the email!
+                        </div>
+            }
+        }
 
         return (
-            <div className="contact">
-                <SectionHeader className="contact__header">CONTACT</SectionHeader>
+            
+                <div className="contact">
+                    <SectionHeader className="contact__header">CONTACT</SectionHeader>
 
-                <Paragraph className="contact__paragraph">
-                    My name is Tomasz Gajda, I’m a third year Applied Computer Science student at
-                    AGH University of Science and Technology in Cracow, Poland.
-                </Paragraph>
+                    <Paragraph className="contact__paragraph">
+                        My name is Tomasz Gajda, I’m a third year Applied Computer Science student at
+                        AGH University of Science and Technology in Cracow, Poland.
+                    </Paragraph>
 
-                <img src={separator} className="contact__separator" alt="contactSeparator"/>
+                    <img src={separator} className="contact__separator" alt="contactSeparator"/>
 
-                <div className="contact__formwrapper">
-                    {form}
+                    <div className="contact__formwrapper">
+                        {form}
+                        {emailResult} 
+                    </div>
+
+                    <Button onClick = {this.contactHandler} className="contact__submit contact__emailSent">SUBMIT</Button>
                 </div>
-
-                <Button onClick = {this.contactHandler} className="contact__submit">SUBMIT</Button>
-
-            </div>
         );
     }
 }
