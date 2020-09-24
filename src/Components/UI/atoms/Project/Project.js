95213d07c4c2a1ff5cdeeef72e311a38c9be821a
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-09-24 10:58:24 +0200

    working on portfolio repsponsiveness
    changed div with project image background into a img tag

--- src/Components/UI/atoms/Project/Project.js
+++ src/Components/UI/atoms/Project/Project.js
@@ -3,7 +3,8 @@ import Button from '../Button/Button';
 import './Project.css';
 
 const Project = props => (
-  <div className="project" style={{backgroundImage: `url(${props.image})`}}>
+  <div className="project">
+    <img className="project__image" src={props.image}/>
     <div className = "project__container">
       <h2 className="project__tags">{props.tags.join(', ')}</h2>
       <h1 className="project__title">{props.name}</h1>
--- src/Components/pages/App/App.js
+++ src/Components/pages/App/App.js
@@ -29,7 +29,6 @@ class App extends React.Component {
 
   render() {
       
-
       /*let loadingScreen = this.state.loading
           ? <LoadingScreen/>
           : null; */
