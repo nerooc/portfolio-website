bf84291acb8e29d99e17864dd0179d64c8a01355
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-09-26 18:18:03 +0200

    added scroll component
    scroll component is an arrow indicator on the landing page, that encourages user to scroll down

---
+++ src/Components/UI/atoms/Scroll/Scroll.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import arrow from '../../../../Images/white-arrow.svg';
+
+const Scroll = props => (
+    <img className = {props.className} src={arrow} alt="arrow"/>
+)
+
+export default Scroll;
--- src/Components/UI/organisms/Hero/Hero.js
+++ src/Components/UI/organisms/Hero/Hero.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Navbar from '../../molecules/Navbar/Navbar';
 import Header from '../../molecules/Header/Header';
+import Scroll from '../../atoms/Scroll/Scroll';
 
 import './Hero.css';
 
-const Hero = props => (
-  <div className="landing-page">
+const Hero = () => (
+  <div className="hero">
     <Navbar />
     <Header />
+    <Scroll className="hero__arrow" altClassName="hero__arrow--scrolled"/>
   </div>
 );
 
